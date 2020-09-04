13b739ef77f2eba2cab753d743561ea8bc4082fd
Ryan McNair <ryan.mcnair@gmail.com>
2020-09-04 17:36:42 -0500

    adding setup files and main functionality

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,9 @@
+import thisTaco from './components/createCard';
+
 import '../styles/main.scss';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  thisTaco.createCard();
 };
 
 init();
